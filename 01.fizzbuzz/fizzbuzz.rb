e9b04804775121aa980bd7afb28958c866d6e5af
f_kt6g <yujihi08@gmail.com>
2025-01-18 10:13:05 +0900

    ループをwhileからeachに変更、==0をzero?メソッドに置換え、行末に改行を追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,13 @@
-num = 1
-while num <= 20
+(1..20).each do |num|
   case
-  when num % 15 == 0
+  when (num % 15).zero?
     puts "FizzBuzz"
-  when num % 5 == 0
+  when (num % 5).zero?
     puts "Buzz"
-  when num % 3 == 0
+  when (num % 3).zero?
     puts "Fizz"
   else
     puts num
   end
-  num += 1
-end 
+end
+
