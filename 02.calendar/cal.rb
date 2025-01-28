f9fda5deb4922d272e7e3096a3fb340983dfdf6e
f_kt6g <yujihi08@gmail.com>
2025-01-28 15:44:56 +0900

    コードの整形と重複していた`date.day.to_s.rjust(2)`を`day_str`に代入

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -7,26 +7,21 @@ params = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
 month = params["m"].to_i
 year = params["y"].to_i
 
-start_date = Date.new(year,month,1) 
-end_date = Date.new(year,month,-1)  
+start_date = Date.new(year, month, 1) 
+end_date = Date.new(year, month, -1)  
 day_of_week = start_date.wday
 
-wdays = ["日 " + "月 " + "火 " + "水 " + "木 " + "金 " + "土 "]
-
-  puts "#{month}月 #{year}".center(20)
-  puts wdays
-  space_per_day = 3
-  print " " * space_per_day * day_of_week
+puts "#{month}月 #{year}".center(20)
+puts "日 月 火 水 木 金 土"
+SPACE_PER_DAY = "   "
+print SPACE_PER_DAY * day_of_week
 
 (start_date..end_date).each do |date|
-
+  day_str = date.day.to_s.rjust(2)
   if date.saturday?
-    puts date.day.to_s.rjust(2)  + "\n"
+    puts day_str
   else
-    print date.day.to_s.rjust(2) + " "
+    print day_str + " "
   end
-
 end
-
 print "\n"
-
